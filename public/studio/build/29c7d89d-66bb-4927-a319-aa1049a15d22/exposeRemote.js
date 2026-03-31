5d1c62f828d9d34f05eb72aa50443a2e76a2bf62
msenkpiel <marco.senkpiel@basilicom.de>
2026-03-31 10:12:55 +0200

    new build

--- public/studio/build/29c7d89d-66bb-4927-a319-aa1049a15d22/exposeRemote.js
+++ public/studio/build/29c7d89d-66bb-4927-a319-aa1049a15d22/exposeRemote.js
@@ -7,7 +7,7 @@
         window.alternativePluginExportPaths = {}
       }
 
-      window.pluginRemotes.pimcore_plugin_system_banner_bundle = "/bundles/pimcorepluginsystembanner/studio/build/af6e3d5f-7b71-4b65-b022-0769b96acd4a/static/js/remoteEntry.js"
+      window.pluginRemotes.pimcore_plugin_system_banner_bundle = "/bundles/pimcorepluginsystembanner/studio/build/29c7d89d-66bb-4927-a319-aa1049a15d22/static/js/remoteEntry.js"
 
       
     
